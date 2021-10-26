84db2808557169afa346873a4cf3f38773e650b3
TheBlind <1043193478@qq.com>
2021-10-26 17:49:21 +0800

    修改输入框过小

--- src/com/theblind/privatenotes/ui/PrivateNotesEditorForm.java
+++ src/com/theblind/privatenotes/ui/PrivateNotesEditorForm.java
@@ -1,22 +1,28 @@
 package com.theblind.privatenotes.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PrivateNotesEditorForm {
     private JEditorPane editorPane1;
     private JPanel panel1;
 
     public JEditorPane getEditorPane1() {
+        panel1.setPreferredSize(new Dimension(200,90));
+        editorPane1.setPreferredSize(new Dimension(200,90));
         return editorPane1;
     }
 
     public void setEditorPane1(JEditorPane editorPane1) {
+
         this.editorPane1 = editorPane1;
     }
 
     public JPanel getPanel1() {
         return panel1;
     }
+
+
 
     public void setPanel1(JPanel panel1) {
         this.panel1 = panel1;
